55d39ebb365a11c36b62d48ba41ffad7805ab4c1
HuseynagaQurbanov <qusipoint@gmail.com>
2025-05-17 15:52:06 +0400

    MainMenu screen bug fixed

---
+++ FlappyBird/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,14 @@
+#nullable enable
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    [SerializeField] private GameObject startScreen;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
